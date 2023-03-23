3002a1a1f9c96b255b3bac8d936cfac41d28c831
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-03-23 13:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function calculateMinCost() {
   //your code here
+	var inputData = document.querySelector("input").value;
+	var newArr = inputData.split(",").map(Number);
+	//alert(typeof newArr);
+	var ans = 0;
+	while (inputArr.length > 1) {
+    var newRope = inputArr[0] + inputArr[1];
+    cost += newRope;
+
+    // delete first two element
+    inputArr.splice(0, 2);
+    inputArr.push(newRope);
+    //alert(newRope);
+  }
+  document.querySelector("#result").textContent = cost;
   
   
   
